01e6de6e653f5aa9f5a6a67ffc1559746dc50c93
jkiviluo <Juha.Kiviluoma@vtt.fi>
2024-11-14 16:59:53 +0200

    Improved Turtle output

--- convert_spinedb_nonbinary.py
+++ convert_spinedb_nonbinary.py
@@ -4,7 +4,7 @@ import spinedb_api as api
 import json
 import toml
 import yaml
-from rdflib import Graph, Namespace, URIRef, RDF, RDFS, Literal, OWL
+from rdflib import Graph, Namespace, URIRef, RDF, RDFS, Literal, BNode, OWL
 
 filedirectory = Path(__file__).parent.resolve()
 os.chdir(filedirectory)
@@ -31,11 +31,11 @@ with api.DatabaseMapping(spinepath) as db_map:
 
     for entity_class in fulldata["entity_classes"]:
         if not entity_class[1]:
-            class_uri = URIRef(str(ines) + entity_class[0])
+            class_uri = URIRef(ines + entity_class[0] + ":")
             g.add((class_uri, RDF.type, OWL.Class))
             #g.add((class_uri, RDFS.label, Literal(entity_class[0])))
         else:
-            nd_class_uri = URIRef(str(ines) + entity_class[0])
+            nd_class_uri = URIRef(ines + entity_class[0])
             g.add((nd_class_uri, RDF.type, OWL.Class))
             for base_class_name in entity_class[1]:
                 base_class = URIRef(ines + base_class_name)
@@ -45,12 +45,13 @@ with api.DatabaseMapping(spinepath) as db_map:
                 g.add((has_base_class, RDFS.range, base_class))
 
     for param_def in fulldata["parameter_definitions"]:
-        has_param = URIRef(ines + param_def[1])
+        class_namespace = Namespace(str(ines) + param_def[0] + ":")
+        has_param = URIRef(class_namespace + ":" + param_def[1])
         class_of_param = URIRef(ines + param_def[0])
         g.add((has_param, RDF.type, OWL.DatatypeProperty))
         g.add((has_param, RDFS.domain, class_of_param))
         g.add((has_param, RDFS.range, RDFS.Literal))  # Range is literal value
-
+        g.add((has_param, RDFS.comment, Literal(param_def[4])))
     g.serialize(destination="ines-spec.ttl")
 
 # the direct conversion from spinedb to yaml causes problems so the conversion is done indirectly through json
