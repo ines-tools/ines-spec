be65c4daa93d10dff0d4dfe037b8b0d11970e85f
jkiviluo <Juha.Kiviluoma@vtt.fi>
2024-11-14 15:23:50 +0200

    Add turtle (RDF/RDFS/OWL) version of data (incomplete at this point)

--- convert_spinedb_nonbinary.py
+++ convert_spinedb_nonbinary.py
@@ -4,6 +4,7 @@ import spinedb_api as api
 import json
 import toml
 import yaml
+from rdflib import Graph, Namespace, URIRef, RDF, RDFS, Literal, OWL
 
 filedirectory = Path(__file__).parent.resolve()
 os.chdir(filedirectory)
@@ -17,13 +18,40 @@ with api.DatabaseMapping(spinepath) as db_map:
     data = {
         k:fulldata[k] for k in [
             "entity_classes",
-            "entity_alternatives",
             "parameter_value_lists",
             "parameter_definitions"
         ]
     }
     with open(jsonpath, 'w') as f:
         json.dump(data, f, indent=4)
+    g = Graph()
+    ines = Namespace("ines-spec:")
+    g.bind("ines", ines)
+    g.bind("owl", OWL)
+
+    for entity_class in fulldata["entity_classes"]:
+        if not entity_class[1]:
+            class_uri = URIRef(str(ines) + entity_class[0])
+            g.add((class_uri, RDF.type, OWL.Class))
+            #g.add((class_uri, RDFS.label, Literal(entity_class[0])))
+        else:
+            nd_class_uri = URIRef(str(ines) + entity_class[0])
+            g.add((nd_class_uri, RDF.type, OWL.Class))
+            for base_class_name in entity_class[1]:
+                base_class = URIRef(ines + base_class_name)
+                has_base_class = URIRef(ines + "has_" + base_class_name)
+                g.add((has_base_class, RDF.type, OWL.ObjectProperty))
+                g.add((has_base_class, RDFS.domain, nd_class_uri))
+                g.add((has_base_class, RDFS.range, base_class))
+
+    for param_def in fulldata["parameter_definitions"]:
+        has_param = URIRef(ines + param_def[1])
+        class_of_param = URIRef(ines + param_def[0])
+        g.add((has_param, RDF.type, OWL.DatatypeProperty))
+        g.add((has_param, RDFS.domain, class_of_param))
+        g.add((has_param, RDFS.range, RDFS.Literal))  # Range is literal value
+
+    g.serialize(destination="ines-spec.ttl")
 
 # the direct conversion from spinedb to yaml causes problems so the conversion is done indirectly through json
 with open(jsonpath, 'r') as f:
