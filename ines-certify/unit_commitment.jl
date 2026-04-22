ab0d8bc1309c22a350c84cc6a10783d9c7041846
tarskul <tars.verschelde@kuleuven.be>
2026-04-22 09:13:25 +0200

    alignment

--- ines-certify/unit_commitment.jl
+++ ines-certify/unit_commitment.jl
@@ -6,7 +6,7 @@ model = JuMP.Model(HiGHS.Optimizer)
 
 s__t = [t for t in 1:10]
 
-p__flow_profile__demand = [round(30.0*(sin(pi/2*t)+1)/2;digits=2) for t in s__t]
+p__flow_profile__demand = [10.0 for t in s__t]#[round(30.0*(sin(pi/2*t)+1)/2;digits=2) for t in s__t]
 
 p__commodity_price__cheap_source = [1.0 for t in s__t]
 p__commodity_price__expensive_source = [10.0 for t in s__t]
@@ -24,16 +24,16 @@ p__capacity__triangle = 100.0
 p__existing_units__triangle = 1.0
 
 p__efficiency__cheap_unit = 0.4
-p__efficiency_min__cheap_unit = 0.2
+p__efficiency_min__cheap_unit = 0.4#0.2
 p__capacity__cheap_unit = 100.0
-p__capacity_min__cheap_unit = 20.0
+p__capacity_min__cheap_unit = 0.0#20.0
 p__existing_units__cheap_unit = 1.0
 p__startup_cost__cheap_unit = 100.0
 p__mut__cheap_unit = 2
 p__mdt__cheap_unit = 1
 
 p__efficiency__expensive_unit = 0.4
-p__efficiency_min__expensive_unit = 0.2
+p__efficiency_min__expensive_unit = 0.4#0.2
 p__capacity__expensive_unit = 100.0
 p__capacity_min__expensive_unit = 0.0
 p__existing_units__expensive_unit = 1.0
