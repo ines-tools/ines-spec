9274084679e3eb073eb58c04875ebae28e7e5e12
tarskul <tars.verschelde@kuleuven.be>
2026-04-27 15:37:05 +0200

    alignment with timeseries

--- ines-certify/rolling_horizon.jl
+++ ines-certify/rolling_horizon.jl
@@ -10,24 +10,30 @@ s__t = 1:10
 
 p__commodity_price__cheap_source = [1.0 for t in s__t]
 p__commodity_price__expensive_source = [10.0 for t in s__t]
-p__investment_cost__cheap_unit = 10000.0
-p__investment_cost__expensive_unit = 10000.0
 p__flow_profile__demand = [10.0 for t in s__t]
-p__efficiency__cheap_link = 0.8
-p__efficiency__expensive_link = 0.8
-p__efficiency__triangle = 0.8
-p__efficiency__cheap_unit = 0.4
-p__efficiency__expensive_unit = 0.4
-p__capacity__cheap_link = 100.0
+
+p__capacity__cheap_link = 1000.0
 p__existing_units__cheap_link = 1.0
-p__capacity__expensive_link = 100.0
+p__efficiency__cheap_link = 0.8
+
+p__capacity__expensive_link = 1000.0
 p__existing_units__expensive_link = 1.0
-p__capacity__triangle = 100.0
+p__efficiency__expensive_link = 0.8
+
+p__capacity__triangle = 1000.0
 p__existing_units__triangle = 1.0
+p__efficiency__triangle = 0.8
+
+p__investment_cost__cheap_unit = 10000.0
 p__capacity__cheap_unit = 100.0#20
 p__existing_units__cheap_unit = 0.0
+p__efficiency__cheap_unit = 0.4
+
+p__investment_cost__expensive_unit = 10000.0
 p__capacity__expensive_unit = 100.0#20
 p__existing_units__expensive_unit = 1.0
+p__efficiency__expensive_unit = 0.4
+
 
 @variable(model,0<=v__flow__cheap_link_in[s__t])
 @variable(model,0<=v__flow__cheap_link_out[s__t]<=p__capacity__cheap_link*p__existing_units__cheap_link)
@@ -79,9 +85,10 @@ println(JuMP.value(v__investment__expensive_unit))
 
 model = JuMP.Model(HiGHS.Optimizer)
 
-p__commodity_price__cheap_source = [1.0 for t in s__t]#10
-p__commodity_price__expensive_source = [10.0 for t in s__t]#1
-p__flow_profile__demand = [10.0 for t in s__t]#20
+p__commodity_price__cheap_source = [10.0 for t in s__t]#10
+p__commodity_price__expensive_source = [1.0 for t in s__t]#1
+p__flow_profile__demand = [20.0 for t in s__t]#20
+
 p__existing_units__cheap_unit = JuMP.value(v__investment__cheap_unit)
 p__existing_units__expensive_unit = JuMP.value(v__investment__expensive_unit)
 
